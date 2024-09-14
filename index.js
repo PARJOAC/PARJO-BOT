bcc2501478a89fb4748783f96811c11433473898
PARJOAC <133354750+PARJOAC@users.noreply.github.com>
2024-09-14 23:50:44 +0200

    Actualizar index.js

--- index.js
+++ index.js
@@ -50,4 +50,74 @@ const Mongo = require("./inicializacion_eventos/mongo.js");
   await Slash(client);
   await Eventos(client);
   await KeepAlive();
+  client.on("ready", async(client) => {
+    async function checkNewVideos(client) {
+  try {
+    const response = await youtube.channels.list({
+      part: 'snippet,contentDetails',
+      id: CHANNEL_ID,
+    });
+
+    const channel = response.data.items[0];
+    if (!channel) {
+      console.log('No se encontró el canal de YouTube.');
+      return;
+    }
+
+    const uploadsPlaylistId = channel.contentDetails.relatedPlaylists.uploads;
+    const playlistItems = await youtube.playlistItems.list({
+      part: 'snippet',
+      playlistId: uploadsPlaylistId,
+      maxResults: 1,
+    });
+
+    const video = playlistItems.data.items[0];
+    if (!video) {
+      console.log('No se encontraron videos en el canal.');
+      return;
+    }
+
+    const videoId = video.snippet.resourceId.videoId;
+
+    const lastVideo = await Video.findOne({}).exec();
+    const lastVideoId = lastVideo ? lastVideo.lastVideoId : null;
+
+    if (videoId !== lastVideoId) {
+      const videoURL = `https://www.youtube.com/watch?v=${videoId}`;
+
+      const channelIdToNotify = '1134282577025441832';
+      const channelToNotify = client.channels.cache.get(channelIdToNotify);
+      if (channelToNotify !== ChannelType.GuildText) {
+        channelToNotify.send({
+          embeds: [new EmbedBuilder()
+            .setTitle(`¡PARJO ACABA DE SUBIR UN VÍDEO!`)
+            .setURL(videoURL)
+            .setDescription(`Título: **${video.snippet.title}**\n\nDescripción: **${video.snippet.description}**`)
+            .setColor("Red")
+          ]
+        })
+      }
+
+      if (lastVideo) {
+        lastVideo.lastVideoId = videoId;
+        await lastVideo.save();
+      } else {
+        await Video.create({ lastVideoId: videoId });
+      }
+    }
+  } catch (error) {
+    console.log('Error al obtener videos del canal de YouTube:', error);
+  }
+}
+
+  client.user.setPresence({
+    activities: [{
+      name: `mejorar el servidor`,
+      type: ActivityType.Competing
+    }],
+    status: 'online',
+  });
+
+  setInterval(() => checkNewVideos(client), 1 * 60 * 1000);
+  })
 })();
